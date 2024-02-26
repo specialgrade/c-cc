c7176f2f445efe39d4dc6b5a38c3b4ddb8bb7114
Issa <aly.mariedelacruz@gmail.com>
2024-02-26 21:53:36 +0800

    add

---
+++ practice-projects-js/arithmetic-operators.js
@@ -0,0 +1,15 @@
+let levelUp = 5;
+let powerLevel = 900;
+let multiplyMe = 40;
+let quarterMe = 1155;
+
+// arithmetic process / formula
+levelUp += 5;  
+powerLevel -= 100  
+multiplyMe *= 11;  
+quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
---
+++ tempCodeRunnerFile.lua
@@ -0,0 +1,9 @@
+let attentionNiSeanKayAly = false; 
+attentionNiSeanKayAly = true;
+if (attentionNiSeanKayAly) {
+    console.log('- - - - - - - - - - - -');
+    console.log('GALIT AKO - ALY FEB. 25, 2024');
+} else {
+    console.log('- - - - - - - - - - - -');
+    console.log('Okay, bati na tayo. - Aly Feb. 25, 2024');
+}
