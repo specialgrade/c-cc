7f50e7b0a006c259ca98e3d5790a6d4d53e3a985
Issa <aly.mariedelacruz@gmail.com>
2024-02-28 22:08:26 +0800

    new

---
+++ practice-projects-js/.pop().js
@@ -0,0 +1,7 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+const removed = chores.pop();
+console.log(chores);
+console.log(removed);
+
+
---
+++ practice-projects-js/.push().js
@@ -0,0 +1,4 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('fix bed', 'sweep floor');
+console.log(chores);
