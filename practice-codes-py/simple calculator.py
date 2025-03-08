0381f87392733f5f859dbbfe86e3e7e0830700b1
Alyssa Marie Dela Cruz <aly.mariedelacruz@gmail.com>
2025-03-08 15:39:39 +0800

    Delete py

--- practice-codes-py/simple calculator.py
+++
@@ -1,9 +0,0 @@
-# simple calculator
-
-firstNum = int(input('Enter first number: ')) 
-secNum = int(input('Enter second number: ')) 
-
-sum = print('The sum is: ', firstNum + secNum) 
-difference = print('The difference is: ', firstNum - secNum) 
-product = print('The product is: ', firstNum * secNum) 
-quotient = print('The quotient is: ', firstNum / secNum) 
--- practice-codes-py/user input.py
+++
@@ -1,3 +0,0 @@
-print('Welcome user!\n')
-name = input('Enter your name: ')
-print('\nThank you for using my program ' + name + '!\n')
